85a97360f8705b9744a059e3c8a6ef054d6824e5
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-02 12:47:09 +0530

    validate all smaple email

--- JavaRegex.java
+++ JavaRegex.java
@@ -1,36 +1,31 @@
 package util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.*;
 
 class JavaRegex {
+    public static void main(String[] args) {
 
-    public static boolean isValidPassword(String Password)
-    {
-        String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[*.!@$%^&].{8}$";
-
-
-        Pattern p = Pattern.compile(regex);
-
-        if (Password == null) {
-            return false;
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> emails = new ArrayList<String>();
+        System.out.println("Enter first how many email you wnts add then enter email addressess to validate");
+        int number=sc.nextInt();
+        for (int i = 0; i < number; i++) {
+            System.out.println("emails");
+            emails.add(sc.next());
         }
 
-        Matcher m = p.matcher(Password);
-        return m.matches();
+        String regex = "^[A-Za-z0-9_-]+(?:\\.[A-Za-z0-9_-])*@[A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        for (String email : emails) {
+            Matcher matcher = pattern.matcher(email);
+            System.out.println(email + " : " + matcher.matches());
+        }
     }
 
-
-    public static void main(String[] args)
-    {
-
-        String str1 = "P@SSWORD";
-        System.out.println(isValidPassword(str1));
-
-
-        String str3 = "P1ss$ord";
-        System.out.println(isValidPassword(str3));
-
-
-        String str5 = "passWord";
-        System.out.println(isValidPassword(str5));
-    }
 }
+
+ 
