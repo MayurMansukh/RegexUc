2406cf3430ac9fff467e7cd43ebc4d511608c876
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-03 13:26:12 +0530

    validate sample emails

--- JavaRegex.java
+++ JavaRegex.java
@@ -1,29 +1,24 @@
 package util;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.regex.*;
 
 class JavaRegex {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> emails = new ArrayList<String>();
-        System.out.println("Enter first how many email you wnts add then enter email addressess to validate");
-        int number=sc.nextInt();
-        for (int i = 0; i < number; i++) {
-            System.out.println("emails");
-            emails.add(sc.next());
-        }
+        public void validateEmail(){
+		String[] email = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com","abc111@yabc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com",
+									"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com",
+									"abc@gmail.com.1a","abc@gmail.com.aa.au"};
 
-        String regex = "^[A-Za-z0-9_-]+(?:\\.[A-Za-z0-9_-])*@[A-Za-z0-9-]+(?:\\.[A-Za-z0-9-]+)*$";
+		for(int i=0; i<email.length; i++) {
+			boolean validEmail = Pattern.compile("^[a-zA-Z0-9]+(?:[\\+._-][a-zA-Z0-9]+)*@[a-zA-Z0-9-]{1,}(?:\\.[a-zA-Z]{2,5})?(?:\\.[a-zA-Z]{2,5})?$").matcher(email[i]).matches();
 
-        Pattern pattern = Pattern.compile(regex);
-
-        for (String email : emails) {
-            Matcher matcher = pattern.matcher(email);
-            System.out.println(email + " : " + matcher.matches());
-        }
+			if(validEmail == true) {
+				System.out.println("\nEmail ID " + email[i] + " is Valid");		}
+			else {
+				System.out.println("\nEmail Id " +email[i] + " is Invalid");
+			}
+		}
+	}
     }
 
 }
